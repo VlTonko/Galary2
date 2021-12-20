b619e85d67e546b29e6293b4d19bf4a6e3197c88
Vladislav Tonkohog <48497062+VlTonko@users.noreply.github.com>
2021-12-20 02:22:19 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import './style.css';
-import elements from './scripts/elements';
-import View from './scripts/view';
-
-let firstClass = new View();
-elements.btnNext.addEventListener('click', firstClass.btnNextSlide);
-elements.btnPrew.addEventListener('click', firstClass.btnPrewSlide);
-elements.btnPlay.addEventListener('click', firstClass.runShow);
-elements.btnStop.addEventListener('click', firstClass.stopShow);
